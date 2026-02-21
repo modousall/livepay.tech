9bf4bf1e8583aee8508d8ade482571904c9ca603
modousall <modousall1@gmail.com>
2026-02-21 16:24:14 +0000

    Add SW kill-switch to clear cache

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// Disable service worker to avoid stale caching issues in production.
+// Temporary: register SW to force uninstall of stale caches.
+if ("serviceWorker" in navigator && import.meta.env.PROD) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => {});
+  });
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
