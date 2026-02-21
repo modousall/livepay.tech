b33e918d7bbf685d6d869fbee2be5a51bf3579a0
modousall <modousall1@gmail.com>
2026-02-21 16:29:18 +0000

    Remove service worker and disable index cache

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// Temporary: register SW to force uninstall of stale caches.
-if ("serviceWorker" in navigator && import.meta.env.PROD) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").catch(() => {});
-  });
-}
+// Service worker disabled.
 
 createRoot(document.getElementById("root")!).render(<App />);
